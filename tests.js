c771844ae9aaeaa53ddac3fb117f01fd0c4ecdc9
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2022-01-20 23:26:25 -0300

    feature (gameplay): Basic gameplay logic plus unit testing with console.assert

---
+++ tests.js
@@ -0,0 +1,29 @@
+console.assert(
+  generateRandomNumber(1, 2) > 0,
+  'Generated numbers exceed the min'
+);
+
+console.assert(
+  generateRandomNumber(1, 2) < 3,
+  'Generated numbers exceed the max range'
+);
+
+console.assert(
+  generateRandomColor(['green', 'red', 'blue', 'yellow'], 2) === 'blue',
+  `Generated color doesn't match`
+);
+
+console.assert(
+  capturePlayerInput({ target: { value: 'red' } }) === 'red',
+  `Captured color doesn't match!`
+);
+
+console.assert(
+  generateRandomColorSequence([], ['green']) === 1,
+  `Sequence can't be generated correctly`
+);
+
+console.assert(
+  evaluatePayerSequence(['red'], ['red'], true) === true,
+  `Player sequence doesn't match game sequence`
+);
