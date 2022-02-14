4baa40cc8e04ad512bc41f4c4e7296877bd3bc2e
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2022-02-14 11:44:47 -0300

    Unit tests

---
+++ cypress/integration/unit/game.spec.js
@@ -0,0 +1,25 @@
+import {
+  getRandomNumber,
+  initializeGameSettings,
+  initializePlayerSettings,
+} from '../../../game';
+
+describe('Game functions', function () {
+  it('Should initialize game properties', function () {
+    const game = initializeGameSettings();
+    expect(game.colors).to.deep.equal(['green', 'red', 'blue', 'yellow']);
+    expect(game.color_sequence).to.be.empty;
+  });
+
+  it('Should initialize player properties', function () {
+    const game = initializePlayerSettings();
+    expect(game.color_sequence).to.be.empty;
+    expect(game.sequence).to.be.equal(1);
+    expect(game.can_continue).to.be.equal(true);
+  });
+
+  it('Should generate a random number between 1 and 4', function () {
+    const result = getRandomNumber(1, 4);
+    expect(result).to.be.within(1, 4);
+  });
+});
